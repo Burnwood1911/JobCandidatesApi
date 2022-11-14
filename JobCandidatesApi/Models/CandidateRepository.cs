dd8f607434efdd7e0f0efbdb7e75b90d6d05e212
Alex Rossi <burnwood1911@gmail.com>
2022-11-14 11:36:04 -0800

    Added candidate repository

---
+++ JobCandidatesApi/Models/CandidateRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace JobCandidatesApi.Models
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        public Task<Candidate> CreateCandidate(Candidate candidate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Candidate> UpdateCandidate(Candidate candidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
