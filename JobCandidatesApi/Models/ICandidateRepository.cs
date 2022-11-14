c1dd1b4687117999ca80291543cef13ac4dfb157
Alex Rossi <burnwood1911@gmail.com>
2022-11-14 11:35:11 -0800

    Added interface for candidate repo

---
+++ JobCandidatesApi/Models/ICandidateRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace JobCandidatesApi.Models
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> CreateCandidate(Candidate candidate);
+        Task<Candidate> UpdateCandidate(Candidate candidate);
+    }
+
+}
